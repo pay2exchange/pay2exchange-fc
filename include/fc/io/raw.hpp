a779ce4f2164ff2e3a7c29d8ae9e9e525092a75b
Abit <abitmore@users.noreply.github.com>
2024-02-06 18:55:15 +0100

    Avoid potential corruption: unpack(static_variant)

--- include/fc/io/raw.hpp
+++ include/fc/io/raw.hpp
@@ -889,8 +889,9 @@ namespace fc {
        --_max_depth;
        unsigned_int w;
        fc::raw::unpack( s, w, _max_depth );
-       sv.set_which(w.value);
-       sv.visit( unpack_static_variant<Stream>( s, _max_depth ) );
+       static_variant<T...> helper( static_cast<typename static_variant<T...>::tag_type>(w.value) );
+       helper.visit( unpack_static_variant<Stream>( s, _max_depth ) );
+       sv = helper;
     }
 
 } } // namespace fc::raw
